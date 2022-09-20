23afee2c0b55858da71febfbffd6c842758ff4c2
Jan Rajm <xrajmj00@stud.feec.vutbr.cz>
2022-09-20 14:15:17 +0200

    zefektivnění morseovky: menší požadavek na paměť

--- demo3/Src/main.c
+++ demo3/Src/main.c
@@ -23,19 +23,23 @@
   #warning "FPU is not initialized, but the project is compiling for an FPU. Please initialize the FPU before use."
 #endif
 
-#define tečka 100000
-#define čárka 3*tečka
+
+
 int main(void)
 {
 	RCC->AHBENR |= RCC_AHBENR_GPIOAEN;
 	GPIOA->MODER |= GPIO_MODER_MODER5_0;
 	GPIOA->BRR = (1<<5); // reset
-	uint8_t pole_znaku[32] = {1, 0, 1, 0, 1, 0, 0, 1, 1, 1, 0, 1, 1, 1, 0, 1, 1, 1, 0, 0, 1, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0};
+	//uint8_t pole_znaku[32] = {1, 0, 1, 0, 1, 0, 0, 1, 1, 1, 0, 1, 1, 1, 0, 1, 1, 1, 0, 0, 1, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0};
+	uint32_t bity_znaku = 0b10101001110111011100101010000000;
+	//uint32_t maska = 0b10000000000000000000000000000000;
     /* Loop forever */
 	while(1){
 		/*for (volatile uint32_t i = 0; i < 100000; i++) {}
 		GPIOA->ODR ^= (1<<5); // toggle
 		for (volatile uint32_t i = 0; i < 100000; i++) {}*/
+
+		/*
 		for(uint8_t i = 0; i < 32; i++){
 			if(pole_znaku[i]){
 				GPIOA->BSRR = (1<<5); // set
@@ -44,6 +48,18 @@ int main(void)
 				GPIOA->BRR = (1<<5); // reset
 			}
 			for (volatile uint32_t i = 0; i < 170000; i++) {}
+			*/
+
+		uint32_t maska = 0b10000000000000000000000000000000;
+		for(uint8_t i = 0; i < 32; i++){
+			if(bity_znaku & maska){
+				GPIOA->BSRR = (1<<5); // set
+			}
+			else{
+				GPIOA->BRR = (1<<5); // reset
+			}
+			for (volatile uint32_t i = 0; i < 170000; i++) {}
+			maska = maska >> 1;
 		}
 	}
 
