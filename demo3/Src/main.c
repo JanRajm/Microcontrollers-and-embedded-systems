ea67e8969bb763d8996d3d0a73faf58ffdde9702
Jan Rajm <xrajmj00@stud.feec.vutbr.cz>
2022-09-20 13:56:41 +0200

    Přidání morseovky

--- demo3/Src/main.c
+++ demo3/Src/main.c
@@ -29,10 +29,22 @@ int main(void)
 {
 	RCC->AHBENR |= RCC_AHBENR_GPIOAEN;
 	GPIOA->MODER |= GPIO_MODER_MODER5_0;
+	GPIOA->BRR = (1<<5); // reset
+	uint8_t pole_znaku[32] = {1, 0, 1, 0, 1, 0, 0, 1, 1, 1, 0, 1, 1, 1, 0, 1, 1, 1, 0, 0, 1, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0};
     /* Loop forever */
 	while(1){
-		for (volatile uint32_t i = 0; i < 100000; i++) {}
+		/*for (volatile uint32_t i = 0; i < 100000; i++) {}
 		GPIOA->ODR ^= (1<<5); // toggle
-		for (volatile uint32_t i = 0; i < 100000; i++) {}
+		for (volatile uint32_t i = 0; i < 100000; i++) {}*/
+		for(uint8_t i = 0; i < 32; i++){
+			if(pole_znaku[i]){
+				GPIOA->BSRR = (1<<5); // set
+			}
+			else{
+				GPIOA->BRR = (1<<5); // reset
+			}
+			for (volatile uint32_t i = 0; i < 170000; i++) {}
+		}
 	}
+
 }
