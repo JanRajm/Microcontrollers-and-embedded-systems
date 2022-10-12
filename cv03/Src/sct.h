654753529870d269e1d0d75539a54b9335a9fba0
Jan Rajm <xrajmj00@stud.feec.vutbr.cz>
2022-10-12 16:46:19 +0200

    cvičení 3

---
+++ cv03/Src/sct.h
@@ -0,0 +1,20 @@
+/*
+ * sct.h
+ *
+ *  Created on: 12. 10. 2022
+ *      Author: xrajmj00
+ */
+
+#ifndef SCT_H_
+#define SCT_H_
+#include <stdint.h>
+#include "stm32f0xx.h"
+	void sct_init(void);
+	void sct_led(uint32_t value);
+	void sct_value(uint16_t value);
+#endif /* SCT_H_ */
+
+// BRR = reset, BSRR = set
+
+
+
