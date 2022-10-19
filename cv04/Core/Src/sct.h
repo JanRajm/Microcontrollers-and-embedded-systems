3edb4fc62309b72a42efb12e2e4e46798c6eb03b
Jan Rajm <xrajmj00@stud.feec.vutbr.cz>
2022-10-19 17:47:32 +0200

    cv4

--- cv04/Core/Src/sct.h
+++ cv04/Core/Src/sct.h
@@ -7,11 +7,11 @@
 
 #ifndef SCT_H_
 #define SCT_H_
-
-void sct_init(void);
-void sct_led(uint32_t value);
-void sct_value(uint16_t value);
-
+#include <stdint.h>
+#include "stm32f0xx.h"
+	void sct_init(void);
+	void sct_led(uint32_t value);
+	void sct_value(uint16_t value, uint8_t led);
 #endif /* SCT_H_ */
 
 // BRR = reset, BSRR = set
